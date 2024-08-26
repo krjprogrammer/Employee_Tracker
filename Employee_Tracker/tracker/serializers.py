5e692e43848109969f9ad748bf2a77a4f1e3f3ee
krjprogrammer <137873227+krjprogrammer@users.noreply.github.com>
2024-08-26 13:11:28 +0530

    Delete Employee_Tracker directory

--- Employee_Tracker/tracker/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import user_data
-class user_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = user_data
-        fields = '__all__'
--- Employee_Tracker/tracker/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import User_Registration,User_Login
-urlpatterns = [
-    path('register',User_Registration.as_view()),
-    path('login',User_Login.as_view()),
-]
